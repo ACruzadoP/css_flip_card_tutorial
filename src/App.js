a13ee1358e510cf66a55f6e1cff1c77138bd5051
Alberto <albertocruzadopleite@gmail.com>
2020-10-24 17:31:28 +0200

    The exercise was successfully completed

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import React from 'react';
 
 function App() {
   return (
-    <div>
-      HOLA MUNDO
+    <div className="container">
+      <div className="card-container">
+        <div className="card">
+          <figure className="front">
+            <h1>PRO TIER</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus, inventore?</p> 
+          </figure>
+          <figure className="back">
+            <h1>PRICE: 20€</h1>
+            <button>Buy</button>
+          </figure>
+        </div>
+      </div>
     </div>
   );
 }
